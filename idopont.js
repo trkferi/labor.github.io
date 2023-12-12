067917178a2a5e0ddcc8bf2f69a0b340013a90dd
trkferi <torok.ferenc-2019@keri.mako.hu>
2023-12-12 11:52:35 +0100

    idopont Feri

---
+++ idopont.js
@@ -0,0 +1,12 @@
+foglal()
+{
+    var nev=document.getElementById("nev").value;
+    var telefonszam=document.getElementById("telefonszam").value;
+    var tajszam=document.getElementById("tajszam").value;
+    var datum=document.getElementById("datum").value;
+    var ido=document.getElementById("ido").value;
+
+document.getElementById("valasz").innerHTML=nev + telefonszam + tajszam;
+
+
+}
