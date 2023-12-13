b0141c6b6a5c59ded1d3c4d53aed8149fe208a8f
trkferi <torok.ferenc-2019@keri.mako.hu>
2023-12-13 08:25:55 +0100

    Feri időpont

--- idopont.js
+++ idopont.js
@@ -1,12 +1,25 @@
-foglal()
+function foglal()
 {
-    var nev=document.getElementById("nev").value;
-    var telefonszam=document.getElementById("telefonszam").value;
-    var tajszam=document.getElementById("tajszam").value;
-    var datum=document.getElementById("datum").value;
-    var ido=document.getElementById("ido").value;
+    
+ 
 
-document.getElementById("valasz").innerHTML=nev + telefonszam + tajszam;
+    var nev=document.getElementById('nev').value;
+    var telefonszam=document.getElementById('telefonszam').value;
+    var tajszam=document.getElementById('tajszam').value;
+    var datum=document.getElementById('datum').value;
+    var ido=document.getElementById('ido').value;
+    var szolgaltatas=document.getElementById('szolgaltatas').value;
+
+    if(nev=="" || telefonszam=="" || tajszam=="" || datum=="" || ido=="" || szolgaltatas=="")
+    {
+        alert("Töltsön ki minden mezőt!");
+    }
+    else
+    {
+
+            alert("Foglalás sikeres! Kedves "+ nev + "! Önnek " + datum + " " + ido +" percre van időpontja " + szolgaltatas + " szolgáltatásra.");
+
+    }
 
 
 }
